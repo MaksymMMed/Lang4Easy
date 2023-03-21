b4ae6beb0f0433d399d7074033ff9d886a349c52
Maksym Mediuh <maksim2003mmm15@gmail.com>
2023-03-21 21:57:26 +0200

    Add DAL and some entities here

---
+++ DAL/Entities/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Exercise
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Answer { get; set; }
+        public int LessonId { get; set; }
+        public Lesson? Lesson { get; set; }
+
+    }
+}
